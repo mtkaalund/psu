0d910671f57cc0eb55752472b97289c8cfcd6c3b
Michael Torp Kaalund <kaalund@gmail.com>
2017-09-07 23:05:40 +0200

    some common bit manipulations

---
+++ src/macros.h
@@ -0,0 +1,9 @@
+#ifndef _MACROS_H_
+#define _MACROS_H_
+
+#define bit_get(p,m) ((p) & (m))
+#define bit_set(p,m) ((p) |= (m))
+#define bit_clear(p,m) ((p) &= ~(m))
+#define bit_flip(p,m) ((p) ^= (m))
+
+#endif
